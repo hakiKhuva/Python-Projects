42a4022fdf6dbb7915db8634a40b0a2075abddfb
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2022-10-29 23:20:15 +0530

    Updated many statements in main.py
    - specifically imported `randint` function
    - changed the position of printing `upper_limit` and `lower_limit`
    - used `for loop` instead `while loop` with variable
    - used `user_guess_flag` to check correct guess
    - add `run again` functionality

--- Guess The Number/main.py
+++ Guess The Number/main.py
@@ -1,29 +1,33 @@
-while True:
-    
-    import random
+from random import randint
 
-   
+while True:   
     lower_limit = int(input("Enter the lower limit: "))
     upper_limit = int(input("Enter upper limit: "))
 
-    random_number = random.randint(lower_limit, upper_limit)
-    print("You will have to choose a number between ", upper_limit, " and ", lower_limit)
+    random_number = randint(lower_limit, upper_limit)
+    print("You will have to choose a number between", lower_limit, "and", upper_limit)
 
-    chances = 0
-    while chances < 8:
-        chances += 1
+    user_guess_flag = False
+
+    for _ in range(8):
         guess = int(input("Enter your guess: "))
-        if random_number == guess:
-            print("Congratulations, you did it. The number was ", random_number)
-            break
-        elif guess < random_number:
+
+        if guess < random_number:
             print("You guessed a small number.")
         elif guess > random_number:
             print("You guessed a large number.")
-        if chances == 7:
-            print("\n You've run out of chances")
-            print("\n The number was ", random_number)
-            print("Better luck next time")
+        else:
+            print("Congratulations, you did it. The number was ", random_number)
+            user_guess_flag = True
             break
-    print("\n")
-    break
+        
+    if user_guess_flag == False:
+        print()
+        print("You've run out of chances")
+        print("The number was ", random_number)
+        print("Better luck next time")
+
+    print()
+    yes_no = input("run again?[y/n] : ").strip().lower()
+    if yes_no in ("n","no"):
+        break
